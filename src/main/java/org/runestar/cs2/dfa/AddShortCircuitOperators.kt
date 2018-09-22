1e7c9c9e106af4a01eb607c82f8910efde74d611
hunterwb <hunter@hunterwb.com>
2018-09-22 12:16:32 -0700

    Fix nested boolean short circuit operators

--- src/main/java/org/runestar/cs2/dfa/AddShortCircuitOperators.kt
+++ src/main/java/org/runestar/cs2/dfa/AddShortCircuitOperators.kt
@@ -14,35 +14,45 @@ object AddShortCircuitOperators : Phase {
     }
 
     private fun ors(func: Func) {
-        val itr = func.insns.iterator()
-        while (itr.hasNext()) {
-            val if1 = itr.next() as? Insn.Branch ?: continue
-            if (!itr.hasNext()) continue
-            val if2 = itr.next() as? Insn.Branch ?: continue
-            if (if1.pass == if2.pass) {
-                itr.remove()
-                if1.expr = Expr.Operation(listOf(Type.BOOLEAN), Opcodes.SS_OR, mutableListOf(if1.expr, if2.expr))
+        outer@
+        while (true) {
+            val itr = func.insns.iterator()
+            while (itr.hasNext()) {
+                val if1 = itr.next() as? Insn.Branch ?: continue
+                if (!itr.hasNext()) continue
+                val if2 = itr.next() as? Insn.Branch ?: continue
+                if (if1.pass == if2.pass) {
+                    itr.remove()
+                    if1.expr = Expr.Operation(listOf(Type.BOOLEAN), Opcodes.SS_OR, mutableListOf(if1.expr, if2.expr))
+                    continue@outer
+                }
             }
+            return
         }
     }
 
     private fun ands(func: Func) {
-        val itr = func.insns.iterator()
-        while (itr.hasNext()) {
-            val if1 = itr.next() as? Insn.Branch ?: continue
-            val if2 = func.insns.next(if1.pass) as? Insn.Branch ?: continue
+        outer@
+        while (true) {
+            val itr = func.insns.iterator()
+            while (itr.hasNext()) {
+                val if1 = itr.next() as? Insn.Branch ?: continue
+                val if2 = func.insns.next(if1.pass) as? Insn.Branch ?: continue
 
-            val else1 = func.insns.next(if1) as? Insn.Goto ?: continue
-            val else2 = func.insns.next(if2) as? Insn.Goto ?: continue
+                val else1 = func.insns.next(if1) as? Insn.Goto ?: continue
+                val else2 = func.insns.next(if2) as? Insn.Goto ?: continue
 
-            if (else1.label == else2.label) {
-                func.insns.remove(if1.pass)
-                func.insns.remove(else2)
-                func.insns.remove(if2)
+                if (else1.label == else2.label) {
+                    func.insns.remove(if1.pass)
+                    func.insns.remove(else2)
+                    func.insns.remove(if2)
 
-                if1.pass = if2.pass
-                if1.expr = Expr.Operation(listOf(Type.BOOLEAN), Opcodes.SS_AND, mutableListOf(if1.expr, if2.expr))
+                    if1.pass = if2.pass
+                    if1.expr = Expr.Operation(listOf(Type.BOOLEAN), Opcodes.SS_AND, mutableListOf(if1.expr, if2.expr))
+                    continue@outer
+                }
             }
+            return
         }
     }
 }
