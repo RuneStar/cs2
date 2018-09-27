db9b2997d7f47d845f3283b7c49a813e03bd7689
hunterwb <hunter@hunterwb.com>
2018-09-26 22:20:48 -0700

    Remove unused functions

--- src/main/java/org/runestar/cs2/util/GraphTraversal.kt
+++ src/main/java/org/runestar/cs2/util/GraphTraversal.kt
@@ -1,17 +1,5 @@
 package org.runestar.cs2.util
 
-fun <N : Any> DirectedGraph<N>.preOrder(
-        n: N = head,
-        seen: MutableSet<N> = HashSet(),
-        consumer: (N) -> Boolean
-) {
-    if (!seen.add(n)) return
-    if (!consumer(n)) return
-    for (s in immediateSuccessors(n)) {
-        preOrder(s, seen, consumer)
-    }
-}
-
 fun <N : Any> DirectedGraph<N>.postOrder(
         n: N = head,
         seen: MutableSet<N> = HashSet(),
@@ -22,32 +10,6 @@ fun <N : Any> DirectedGraph<N>.postOrder(
         postOrder(s, seen, consumer)
     }
     consumer(n)
-}
-
-fun <N : Any> DirectedGraph<N>.isPredecessor(
-        n: N,
-        predecessor: N,
-        seen: MutableSet<N> = HashSet()
-): Boolean {
-    for (p in immediatePredecessors(n)) {
-        if (!seen.add(p)) continue
-        if (p == predecessor) return true
-        if (isPredecessor(p, predecessor, seen)) return true
-    }
-    return false
-}
-
-fun <N : Any> DirectedGraph<N>.isSuccessor(
-        n: N,
-        successor: N,
-        seen: MutableSet<N> = HashSet()
-): Boolean {
-    for (s in immediateSuccessors(n)) {
-        if (!seen.add(s)) continue
-        if (s == successor) return true
-        if (isSuccessor(s, successor, seen)) return true
-    }
-    return false
 }
 
 fun <N : Any> DirectedGraph<N>.isSuccessorAcyclic(
