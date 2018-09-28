b3fc3e9898a92d33905aa166b9c6c73e1179f248
hunterwb <hunter@hunterwb.com>
2018-09-28 13:19:14 -0700

    Add clan_isfriend/ignore opcode names

--- src/main/java/org/runestar/cs2/Opcodes.java
+++ src/main/java/org/runestar/cs2/Opcodes.java
@@ -355,8 +355,8 @@ public final class Opcodes {
     public static final int IGNORE_TEST = 3623;
     public static final int CLAN_ISSELF = 3624;
     public static final int CLAN_GETCHATOWNERNAME = 3625;
-    public static final int _3626 = 3626;
-    public static final int _3627 = 3627;
+    public static final int CLAN_ISFRIEND = 3626;
+    public static final int CLAN_ISIGNORE = 3627;
     public static final int _3628 = 3628;
     public static final int _3629 = 3629;
     public static final int _3630 = 3630;
