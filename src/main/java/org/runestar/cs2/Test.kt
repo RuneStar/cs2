ebab906ab36f691d0c24c45c6a0b4dbe335ad37f
hunterwb <hunter@hunterwb.com>
2018-12-11 04:41:47 -0800

    Write files multi threaded
    Cuts total time in half

--- src/main/java/org/runestar/cs2/Test.kt
+++ src/main/java/org/runestar/cs2/Test.kt
@@ -2,21 +2,33 @@ package org.runestar.cs2
 
 import java.nio.file.Files
 import java.nio.file.Paths
+import java.time.Duration
+import java.time.Instant
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 fun main(args: Array<String>) {
+    val start = Instant.now()
 
     val loadDir = Paths.get("input")
     val saveDir = Paths.get("scripts")
     Files.createDirectories(saveDir)
-
     val decompiler = Decompiler(Loader.Scripts(loadDir))
+    val io = Executors.newCachedThreadPool()
 
     loadDir.toFile().list().forEach { fileName ->
         val scriptId = fileName.toInt()
         val scriptName = Loader.SCRIPT_NAMES.load(scriptId) ?: fileName
         println("$scriptId $scriptName")
-        val s = decompiler.decompile(scriptId)
+        val decompiled = decompiler.decompile(scriptId)
         val saveFile = saveDir.resolve("$scriptName.cs2")
-        Files.write(saveFile, s.toByteArray())
+        io.submit {
+            Files.write(saveFile, decompiled.toByteArray())
+        }
     }
+
+    io.shutdown()
+    io.awaitTermination(Long.MAX_VALUE, TimeUnit.HOURS)
+
+    println(Duration.between(start, Instant.now()))
 }
